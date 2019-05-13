6ec4ed1ede6aee03ac79009da821492e9e398f02
FabioViegas6 <fabioviegas16.fv@gmail.com>
2019-05-13 02:05:31 +0100

    criação da função getContentValues na class cor

--- app/src/main/java/com/example/colecaosapatos/Cor.java
+++ app/src/main/java/com/example/colecaosapatos/Cor.java
@@ -1,15 +1,18 @@
 package com.example.colecaosapatos;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
 public class Cor {
 
-    private int id;
+    private long id;
     private String nomeC;
 
-    public int getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -20,4 +23,32 @@ public class Cor {
     public void setNomeC(String nomeC) {
         this.nomeC = nomeC;
     }
+
+
+    public ContentValues getContentValues() {
+        ContentValues valores = new ContentValues();
+
+        valores.put(BdTableCor.CAMPO_NOME_COR, nomeC);
+
+        return valores;
+    }
+
+    public static Cor fromCursor(Cursor cursor) {
+        long id = cursor.getLong(
+                cursor.getColumnIndex(BdTableCor._ID)
+        );
+
+        String nomeC = cursor.getString(
+                cursor.getColumnIndex(BdTableCor.CAMPO_NOME_COR)
+        );
+
+        Cor cor = new Cor();
+
+        cor.setId(id);
+        cor.setNomeC(nomeC);
+
+        return cor;
+    }
+
+
 }
